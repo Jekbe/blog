0c7a6e5e5e77e749860ca744f1d7d753a30da3e0
Jekbe <136848979+Jekbe@users.noreply.github.com>
2023-09-08 01:27:30 +0200

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,1 +1,67 @@
-<?php
+session_start();
+$conn = new mysqli("localhost", "root", "", "blog");
+    if ($conn -> connect_error){
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+if (isset($_POST["login"])) {
+            $login = $_POST["name"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $confirm_password = $_POST["confirm_password"];
+    $is_artist = isset($_POST["is_artist"]) ? 1 : 0;
+
+        $hashed_password = md5($password);
+
+
+        $sql = "INSERT INTO Uzytkownicy (Imie, Adres_email, Haslo, Jest_artysta) VALUES ('$login', '$email', '$hashed_password', '$is_artist')";
+        $result = $conn->query($sql);
+
+        if ($result) {
+            echo "<div class='form'>
+                <h3>Zostałeś pomyślnie zarejestrowany.</h3><br/>
+                <p class='link'>Kliknij tutaj, aby się <a href='Login.php'>zalogować</a></p>
+                </div>";
+        } else {
+            echo "<div class='form'>
+                <h3>Nie wypełniłeś wymaganych pól lub wystąpił błąd.</h3><br/>
+                <p class='link'>Kliknij tutaj, aby ponowić próbę <a href='Registration.php'>rejestracji</a>.</p>
+                </div>";
+        }
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rejestracja</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Rejestracja</h1>
+        <form action="register.php" method="POST">
+            <div class="form-group">
+                <label for="name">Nick:</label>
+                <input type="text" id="name" name="nick" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Hasło:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+          <div class="form-group">
+                <input type="checkbox" id="is_artist" name="is_artist">
+                <label for="is_artist">Jestem artystą</label>
+            </div>
+            <div class="form-group">
+                <button type="submit">Zarejestruj</button>
+            </div>
+        </form>
+        <p>Masz już konto? <a href="login.php">Zaloguj się</a></p>
+    </div>
+</body>
+</html>
