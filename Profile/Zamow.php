3b1fb83cf1a0e54995aa6a7b48edd6b10656bc9f
Jekbe <patrykrusakowicz@gmail.com>
2024-06-18 12:31:33 +0200

    nie wiem co tu dalej pisać

--- Profile/Zamow.php
+++ Profile/Zamow.php
@@ -1,1 +1,29 @@
 <?php
+    global $conn;
+
+    include '../Additional/Session.php';
+    include '../Additional/Database con.php';
+?>
+
+<!doctype html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Złóż zamówienie</title>
+    <link rel="stylesheet" href="../Style/Style.css">
+</head>
+<body>
+    <?php include '../Additional/Header.php' ?>
+
+    <section class="zamow container">
+
+    </section>
+
+    <section class="powrot container">
+        <a href="../Index.php">Powrót</a>
+    </section>
+</body>
+</html>
